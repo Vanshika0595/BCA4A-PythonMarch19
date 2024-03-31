6466478718e1247b7c20065db203d6e43e4bcaea
Vanshika Malhotra <128216224+Vanshika0595@users.noreply.github.com>
2024-03-31 12:22:46 +0530

    Add files via upload

---
+++ PYTHON ASSIGNMENT/2.2.py
@@ -0,0 +1,9 @@
+#WAP to demonstrate built in functions of Strings
+s = "Vanshika Malhotra"
+print(s.upper())
+print(s.lower())
+print(s.split())
+print(s.strip())
+print(s.count("a"))
+print(s.find("a"))
+print(s.replace("a", "e"))
